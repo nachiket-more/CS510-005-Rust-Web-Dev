ef1d02da7796daae3a4ee1b0ae5828f0664ca623
Nachiket More <nmore@pdx.edu>
2024-06-11 02:36:01 -0700

    added css styling and updated files

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,10 +1,11 @@
-use std::convert::Infallible;
-use warp::{http::Response, reject::custom, Rejection, Filter};
+use warp::{http::Response, reject::custom, Filter, Rejection};
 
 #[tokio::main]
 async fn main() {
     // Define a route to serve index.html at the root endpoint
     let index_route = warp::path::end().and(warp::fs::file("index.html"));
+
+    let css_route = warp::path("index.css").and(warp::fs::file("index.css"));
 
     // Define a route to fetch data from the backend API
     let questions_route = warp::path("questions")
@@ -17,8 +18,8 @@ async fn main() {
         });
 
     // Combine the routes and run the server
-    let routes = index_route.or(questions_route);
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    let routes = index_route.or(questions_route).or(css_route);
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
 
 #[derive(Debug)]
